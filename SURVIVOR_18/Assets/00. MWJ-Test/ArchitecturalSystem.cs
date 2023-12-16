1bc5f078ef1190a89699a524518bcd8cdeed2323
uhbbang3 <ansdnjswjd33@gmail.com>
2023-12-16 17:38:31 +0900

    [🛠️Update] base Architecture System Update
    청사진 취소 구현 (Esc)
    
    그리드 맞춰 청사진 위치 제한
    - position 가능 범위 float -> int

--- SURVIVOR_18/Assets/00. MWJ-Test/ArchitecturalSystem.cs
+++ SURVIVOR_18/Assets/00. MWJ-Test/ArchitecturalSystem.cs
@@ -1,48 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ArchitecturalSystem : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private GameObject tempPrefab;
-    [SerializeField] Material previewMat;
+    [SerializeField] private Material previewMat;
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private int raycastRange;
+    [SerializeField] private float yGridSize = 0.1f;
 
     private Material originMat;
     private GameObject obj;
     private bool isHold = false;
-    private LayerMask groundLayer = 6;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !isHold)
-        {
-            isHold = true;
-            CreateBluePrintObject(RaycastHitPos().point);
-        }
-
         if (isHold)
         {
-            RaycastHit hit = RaycastHitPos();
-            if (hit.collider != null && hit.collider.gameObject.layer == groundLayer)
-                obj.transform.position = hit.point;
+            RaycastHit hit = RaycastHit();
+            if (hit.collider != null
+                && groundLayer.value == (groundLayer.value | (1 << hit.collider.gameObject.layer)))
+                SetObjPos(hit.point);
 
             if (Input.GetMouseButtonDown(0))
             {
                 SetInitialObject();
                 isHold = false;
             }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Destroy(obj);
+                isHold = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            isHold = true;
+            CreateBluePrintObject(RaycastHit().point);
         }
     }
 
-    private RaycastHit RaycastHitPos()
+    private RaycastHit RaycastHit()
     {
         RaycastHit hit;
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         Physics.Raycast(ray, out hit, raycastRange);
 
         return hit;
+    }
+
+    private void SetObjPos(Vector3 hitPos)
+    {
+        Vector3 _location = hitPos;
+        // Round -> 실수를 정수로 반올림하는 함수
+        _location.Set(Mathf.Round(_location.x), Mathf.Round(_location.y / yGridSize) * yGridSize, Mathf.Round(_location.z));
+
+        obj.transform.position = _location;
     }
 
     private void CreateBluePrintObject(Vector3 pos)
@@ -55,7 +71,8 @@ public class ArchitecturalSystem : MonoBehaviour
     private void SetInitialObject()
     {
         obj.GetComponentInChildren<MeshRenderer>().material = originMat;
-        
-        //obj.transform.GetChild(0).gameObject.layer = groundLayer;
+
+        //obj.layer = groundLayer.value;
+        //obj.transform.GetChild(0).gameObject.layer = groundLayer.value;
     }
 }
