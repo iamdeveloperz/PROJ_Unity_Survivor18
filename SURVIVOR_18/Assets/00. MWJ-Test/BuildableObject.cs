dab1aba65d5ea5b5c10b39fbfa904010555aef9c
uhbbang3 <ansdnjswjd33@gmail.com>
2023-12-18 15:58:08 +0900

    [➕Add] Non-Buildable System
    건축물의 Collider끼리 겹칠 때 설치 못하도록 구현
    - 설치 불가능 상태: 빨간색 Material
    - 설치 가능 상태: 파란색 Material
    - 코드 리팩토링 필요

--- SURVIVOR_18/Assets/00. MWJ-Test/BuildableObject.cs
+++ SURVIVOR_18/Assets/00. MWJ-Test/BuildableObject.cs
@@ -1,21 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildableObject : MonoBehaviour
 {
-    void Start()
+    public event Action OnRedMatAction;
+    public event Action OnBluePrintMatAction;
+    
+    [SerializeField] private Material _redMat;
+    [SerializeField] private Material _bluePrintMat;
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Collider _thisObjectAnotherColldier;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other != _thisObjectAnotherColldier)
+        {
+            _renderer.material = _redMat;
+            OnRedMatAction?.Invoke();
+        }
     }
 
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other != _thisObjectAnotherColldier)
+        {
+            _renderer.material = _redMat;
+            OnRedMatAction?.Invoke();
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        
+        _renderer.material = _bluePrintMat;
+        OnBluePrintMatAction?.Invoke();
     }
 }
