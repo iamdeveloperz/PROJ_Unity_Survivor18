66e34bde884e1af2e27e414b2adc48ab10d2282f
MissHarvest <abcdxsky@naver.com>
2023-12-18 09:43:45 +0900

    [import] 애니메이션 추가
    # 낚시, 채집, 제작, 채굴
    
    # 만약, Material 이 분홍색으로 보이시면 다음과 같이 시도해보세요.
    1. Kevin lglesias / Villager Animations / Materials / VillagerMaterial 클릭
    2. Edit / Rendering / Material / Convert XXX

--- SURVIVOR_18/Assets/02. Scripts/UI/PlayerConditionsUI.cs
+++ SURVIVOR_18/Assets/02. Scripts/UI/PlayerConditionsUI.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PlayerConditionsUI : MonoBehaviour
+public class PlayerConditionsUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private ConditionUI _hpUI;
     private ConditionUI _hungerUI;
@@ -29,5 +31,40 @@ public class PlayerConditionsUI : MonoBehaviour
         playerStatHandler.Moisture.OnRecovered += ((x) => { staminaBlock.SetActive(false); });
         _staminaUI.SetMaximum((int)playerStatHandler.Stamina.maxValue);
         playerStatHandler.Stamina.OnUpdated = _staminaUI.UpdateBar;        
+    }
+
+    public void OnBeginDrag(PointerEventData ped)
+    {
+        Debug.Log("OnBeginDrag");
+        var go = Instantiate(Resources.Load<GameObject>("UI/Image"));
+        go.transform.SetParent(GameObject.Find("Canvas").transform);
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        Debug.Log("OnDrag");
+
+    }
+
+    public void OnEndDrag(PointerEventData ped)
+    {
+        Debug.Log("OnEndDrag");
+
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("OnMouseDown");
+
+    }
+
+    private void OnMouseDrag()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        
     }
 }
