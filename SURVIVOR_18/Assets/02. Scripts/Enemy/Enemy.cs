af351045dfa18251ace01c4889df3a7095fa103d
oteosuk <oteosuk@naver.com>
2023-12-19 23:51:43 +0900

    [💡Feature] - 좀비 및 애니메이션 에셋 추가
    쫓아오는 적에 적용했습니다.

--- SURVIVOR_18/Assets/02. Scripts/Enemy/Enemy.cs
+++ SURVIVOR_18/Assets/02. Scripts/Enemy/Enemy.cs
@@ -9,27 +9,27 @@ public class Enemy : MonoBehaviour
     public int curHealth;
     public Transform target;
 
-    Rigidbody rigid;
-    CapsuleCollider capsuleCollider;
-    Material mat;
+    //Rigidbody rigid;
+    //CapsuleCollider capsuleCollider;
+    //Material mat;
     NavMeshAgent nav;
+    Animator anim;
 
 
     void Awake()
     {
-        rigid = GetComponent<Rigidbody>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
-        mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
+        //rigid = GetComponent<Rigidbody>();
+        //capsuleCollider = GetComponent<CapsuleCollider>();
+        //mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         nav.SetDestination(target.position);
-        // 에이전트가 이동 중인 경우
         if (nav.hasPath)
         {
-            // 이동 방향으로 회전
             RotateTowardsTarget(nav.steeringTarget);
         }
     }
@@ -41,15 +41,5 @@ public class Enemy : MonoBehaviour
         Vector3 direction = (targetPosition - transform.position).normalized;
         Quaternion toRotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, Time.deltaTime * 5f); // 회전 속도를 조절
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-
-
-
-        }
     }
 }
