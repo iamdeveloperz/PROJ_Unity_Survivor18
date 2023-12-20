57a34f691ed2a53de17bc2c01c3b6a07db09a302
oteosuk <oteosuk@naver.com>
2023-12-20 17:34:49 +0900

    [💡Feature] - 좀비스포너 수정

--- SURVIVOR_18/Assets/ZombieSpawner.cs
+++ SURVIVOR_18/Assets/ZombieSpawner.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ZombieSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] zombiePrefabs;
+    public Transform[] spawnLocations;
+    public float spawnInterval = 30f;
+
     void Start()
     {
-        
+        SpawnZombie();
+        InvokeRepeating("SpawnZombie", spawnInterval, spawnInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnZombie()
     {
-        
+        if (zombiePrefabs.Length == 0)
+        {
+            Debug.LogWarning("좀비 프리팹이 할당되어 있지 않습니다.");
+            return;
+        }
+
+        if (spawnLocations.Length == 0)
+        {
+            Debug.LogWarning("위치 프리팹이 할당되어 있지 않습니다.");
+            return;
+        }
+
+        // 랜덤한 좀비 프리팹 선택
+        GameObject selectedZombiePrefab = zombiePrefabs[Random.Range(0, zombiePrefabs.Length)];
+
+        // 랜덤한 위치 선택
+        Transform selectedSpawnLocation = spawnLocations[Random.Range(0, spawnLocations.Length)];
+
+        // 좀비 생성
+        Instantiate(selectedZombiePrefab, selectedSpawnLocation.position, Quaternion.identity);
     }
 }
